e56f56b6b44016664b49f8145b01072d8885514d
Ahmadsafikhan <safionair@gmail.com>
2023-11-27 14:15:49 +0500

    make changes in navabr

--- market-place/src/components/TopNav.js
+++ market-place/src/components/TopNav.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 const TopNav = () => {
   return (
     <div className="bg-gray-100 flex justify-evenly p-4">
+      <div className="bg-gray-300">
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
+      </div>
     </div>
   );
 };
