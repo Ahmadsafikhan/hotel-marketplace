a375fde92c00f8c53bcdc54cb9eb0df3fb05afcc
Ahmadsafikhan <safionair@gmail.com>
2023-11-27 15:01:28 +0500

    changes in server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 // const express = require("express");
-import express from "express"
+import express from "express";
+import fs from "fs/promises";  // Import fs using ESM syntax
 
 const app = express();
-app.get("/api/:message", (req, res) => {
-  res.status(200).send(req.params.message);
-});
 
-app.listen(8000, ()=> console.log("app running on port 8000"))
+// route middleware
+(async () => {
+    const files = await fs.readdir("./routes");
+    files.map(async (r) => {
+        const module = await import(`./routes/${r}`);
+        app.use("/api", module.default);
+    });
+
+    app.listen(8000, () => console.log("app running on port 8000"));
+})();
